2299504d8353186883bf8b13f9e787883325ef3e
tgasser-nv <200644301+tgasser-nv@users.noreply.github.com>
2025-09-22 13:35:34 -0500

    Revert "Dummy commit to set up the chore/type-clean-guardrails PR and branch"
    This reverts commit 71d00f083fb59bda34c82b82eea85602c1710265.

--- nemoguardrails/actions/llm/generation.py
+++ nemoguardrails/actions/llm/generation.py
@@ -130,7 +130,7 @@ class LLMGenerationActions:
             self._init_flows_index(),
         )
 
-    def _extract_user_message_example(self, flow: Flow) -> None:
+    def _extract_user_message_example(self, flow: Flow):
         """Heuristic to extract user message examples from a flow."""
         elements = [item for item in flow.elements if item["_type"] != "doc_string_stmt" and item["_type"] != "stmt"]
         if len(elements) != 2:
